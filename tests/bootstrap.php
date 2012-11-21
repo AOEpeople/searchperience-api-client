3bbfbd1c4504705b9e6cf348d3733ad3edf1478e
Michael Klapper <development@morphodo.com>
2012-11-21 15:11:22 +0100

    Remove composer install check

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,12 +8,6 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
-// Ensure that composer has installed all dependencies
-if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
-	die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
-		. "See http://getcomposer.org for help with installing composer\n");
-}
-
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 // Include the composer autoloader
