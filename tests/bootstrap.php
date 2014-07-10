9e92bcf390b9cc38616f1fe33fade25ec5119e78
pavlo.bogomolenko <pavlo.bogomolenko@aoemedia.de>
2014-07-10 15:17:41 +0200

    #37606 updated phpunit dependency to newer version

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,5 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
-require_once 'PHPUnit/TextUI/TestRunner.php';
-
 // Include the composer autoloader
 require dirname(__DIR__) . '/vendor/autoload.php';
