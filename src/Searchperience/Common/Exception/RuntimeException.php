52332bc44832244840d7263722762d5e9301a28a
timo.schmidt <timo-schmidt@gmx.net>
2014-08-13 17:31:19 +0200

    Refactoring: moved setResponse and getRepsonse to shared base classs

--- src/Searchperience/Common/Exception/RuntimeException.php
+++ src/Searchperience/Common/Exception/RuntimeException.php
@@ -8,4 +8,22 @@ namespace Searchperience\Common\Exception;
  * @time: 09:58
  */
 class RuntimeException extends \RuntimeException implements SearchperienceException {
+	/**
+	 * @var \Guzzle\Http\Message\Response
+	 */
+	protected $response;
+
+	/**
+	 * @param \Guzzle\Http\Message\Response $response
+	 */
+	public function setResponse($response) {
+		$this->response = $response;
+	}
+
+	/**
+	 * @return \Guzzle\Http\Message\Response
+	 */
+	public function getResponse() {
+		return $this->response;
+	}
 }
--- src/Searchperience/Common/Http/Exception/ServerErrorResponseException.php
+++ src/Searchperience/Common/Http/Exception/ServerErrorResponseException.php
@@ -5,23 +5,4 @@ namespace Searchperience\Common\Http\Exception;
 /**
  * Exception when a server error is encountered (5xx codes)
  */
-class ServerErrorResponseException extends \Searchperience\Common\Exception\RuntimeException implements \Searchperience\Common\Exception\SearchperienceException {
-	/**
-	 * @var \Guzzle\Http\Message\Response
-	 */
-	protected $response;
-
-	/**
-	 * @param \Guzzle\Http\Message\Response $response
-	 */
-	public function setResponse($response) {
-		$this->response = $response;
-	}
-
-	/**
-	 * @return \Guzzle\Http\Message\Response
-	 */
-	public function getResponse() {
-		return $this->response;
-	}
-}
+class ServerErrorResponseException extends \Searchperience\Common\Exception\RuntimeException implements \Searchperience\Common\Exception\SearchperienceException {}
