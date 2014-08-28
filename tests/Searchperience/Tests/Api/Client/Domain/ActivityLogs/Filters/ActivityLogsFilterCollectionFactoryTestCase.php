75e66e168c55a767c62cb44e3529e04230a603a2
Nikolay Diaur <nikolay.diaur@aoe.com>
2014-08-28 13:22:34 +0200

    fix test

--- tests/Searchperience/Tests/Api/Client/Domain/ActivityLogs/Filters/ActivityLogsFilterCollectionFactoryTestCase.php
+++ tests/Searchperience/Tests/Api/Client/Domain/ActivityLogs/Filters/ActivityLogsFilterCollectionFactoryTestCase.php
@@ -42,15 +42,15 @@ class ActivityLogsFilterCollectionFactoryTestCase extends \Searchperience\Tests\
 	public function canBuildFilterCollectionFromArguments() {
 		$arguments = array(
 				'logTime' => array(
-						'logtimeStart' => $this->getUTCDateTimeObject('2014-01-01 09:00:00'),
-						'logtimeEnd' => $this->getUTCDateTimeObject('2014-01-01 10:00:00')
+						'logtimeStart' => \DateTime::createFromFormat('Y-m-d H:i:s', '2014-01-01 09:00:00', new \DateTimeZone('UTC')),
+						'logtimeEnd' => \DateTime::createFromFormat('Y-m-d H:i:s', '2014-01-01 10:00:00', new \DateTimeZone('UTC'))
 				),
 				'query' => array('queryString' => 'api', 'queryFields' => 'id,message,classname,methodname,processid,tag'),
 		);
 
 		$filterCollection = $this->instance->createFromFilterArguments($arguments);
 
-		$this->assertEquals('&logtimeStart=2014-01-01%2010%3A00%3A00&logtimeEnd=2014-01-01%2009%3A00%3A00&query=api&queryFields=id%2Cmessage%2Cclassname%2Cmethodname%2Cprocessid%2Ctag', $filterCollection->getFilterStringFromAll(), 'Could not build filterCollection with expected filter string');
+		$this->assertEquals('&logtimeStart=2014-01-01%2009%3A00%3A00&logtimeEnd=2014-01-01%2010%3A00%3A00&query=api&queryFields=id%2Cmessage%2Cclassname%2Cmethodname%2Cprocessid%2Ctag', $filterCollection->getFilterStringFromAll(), 'Could not build filterCollection with expected filter string');
 	}
 
 	/**
