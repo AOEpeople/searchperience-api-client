c440211f5185472690b190f001d2e2963bfd3a33
Alexander Voigt <alexander.voigt@aoe.com>
2016-10-21 09:30:34 +0200

    Add commands that mark all documents for processing or deletion

---
+++ src/Searchperience/Api/Client/Domain/Command/MarkAllDocumentsForDeletionCommand.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Searchperience\Api\Client\Domain\Command;
+
+/**
+ * Class MarkAllDocumentsForDeletionCommand
+ * @package Searchperience\Api\Client\Domain\Command
+ */
+class MarkAllDocumentsForDeletionCommand extends AbstractCommand {
+
+	/**
+	 * @var string
+	 */
+	protected $name = 'MarkAllDocumentsForDeletion';
+}
---
+++ src/Searchperience/Api/Client/Domain/Command/MarkAllDocumentsForProcessingCommand.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Searchperience\Api\Client\Domain\Command;
+
+/**
+ * Class MarkAllDocumentsForProcessingCommand
+ * @package Searchperience\Api\Client\Domain\Command
+ */
+class MarkAllDocumentsForProcessingCommand extends AbstractCommand {
+
+	/**
+	 * @var string
+	 */
+	protected $name = 'MarkAllDocumentsForProcessing';
+}
